8b04e75485dfbd15de00f33f876901a4d1f046ed
Alezito2008 <>
2025-09-26 10:20:58 -0300

    Correr solo si se buildea exitosamente

--- build.sh
+++ build.sh
@@ -4,11 +4,18 @@ mkdir -p build
 cd build
 cmake ..
 cmake --build .
+
+BUILD_STATUS=$?
+
 cd ..
 
 # Si no existe compile_commands.json crear un symlink
 if [ ! -f "compile_commands.json" ]; then
     ln -s ./build/compile_commands.json
+fi
+
+if [ $BUILD_STATUS -ne 0 ]; then
+    exit
 fi
 
 # Usando ./build.sh <ECS | Editor> se ejecuta el target 
